a76bd13984d5dc60828f531f0b25a9a31f1160ce
Dom Chambers <dom.chambers@envitia.com>
2024-10-09 10:14:44 +0100

    add todays sql solution

--- largest_number.py
+++ largest_number.py
@@ -22,20 +22,21 @@ Constraints:
 1 <= nums.length <= 100
 0 <= nums[i] <= 109"""
 
-# plan: 
-# we can beat the naive approach of creating all n! combinations by observing for small k, and reasonable n:  sum(k!) << n!
-# we look through nums to find the elements with the highest first digit i.e. if we have [3,5,9,99,91] then we pick out the numbers 9, 99 and 91.
-# we then create all 3! numbers and pick the largest, and repeat this process until completion. note that for any particular iteration:
-# there will be at most 21 factorial cases which is quite a lot but this process willl be refined once we have the rough version working.
 
 # functions: 
-# create_all_numbers, creates all possible numbers for a list of digits. (to be amended)
+# create_max_num creates the maximum number.
 # get next_largest_first_digits, given a list, find all the numbers with highest first digit, remove them from the list and move them to another a list.
 
 
 
 def create_max_num(numbers:list) -> str:
-    return str(max([int(''.join(map(str,permutation))) for permutation in permutations(numbers)]))
+    numbers = [str(num) for num in numbers]
+    one_digit_num = [str(num) for num in sorted([int(num) for num in numbers if len(num) == 1],reverse=True)]
+    two_digit_num = [str(num) for num in sorted([int(num) for num in numbers if len(num) == 2],reverse=True)]
+    three_digit_num = [str(num) for num in sorted([int(num) for num in numbers if len(num) == 3],reverse=True)]
+    print(one_digit_num+two_digit_num+three_digit_num)
+    return "".join(one_digit_num+two_digit_num+three_digit_num)
+
 
 def get_next_largest_digit(nums: list) -> list:
     highest_first_digit = max([int(str(num)[0]) for num in nums])
